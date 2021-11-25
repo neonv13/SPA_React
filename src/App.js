6fab587a157e1ad5a239621cfc4b091d449f4ea6
neonv13 <neonv13@users.noreply.github.com>
2021-11-25 10:45:53 +0100

    New project and setup datafile

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Just a page</h1>
+    </div>
+  );
+}
+
+export default App;
+
+//npx json-server --watch data/db.json --port 8000
